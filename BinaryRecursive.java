f5895b875d598b2fbc602f215205138f4e69e517
Lavanya-Pachipalaa <lavanyapachipala12@gmail.com>
2025-10-22 18:57:20 +0530

    Add files via upload

---
+++ BinaryRecursive.java
@@ -0,0 +1,43 @@
+//Java code to implement binary search (recursive)
+
+import java.util.Scanner;
+
+public class BinaryRecursive {
+    public static int binary_recursive(int arr[], int key, int start, int end) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        } else if (arr[mid] > key) {
+            return binary_recursive(arr, key, start, mid - 1);
+        } else {
+            return binary_recursive(arr, key, mid + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[100];
+        System.out.print("Enter no.of elements in array:");
+        int n = sc.nextInt();
+        System.out.print("Enter elements of array in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the key element to search:");
+        int key = sc.nextInt();
+        int start = 0, end = n - 1;
+        int index = binary_recursive(arr, key, start, end);
+        if (index == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println(key + " found at index " + index);
+        }
+        sc.close();
+    }
+}
