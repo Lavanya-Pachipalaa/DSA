6edc072f963e5d6b4d8d02760d0bfc41b7e7d3c1
Lavanya-Pachipalaa <lavanyapachipala12@gmail.com>
2025-10-22 08:23:43 +0530

    Add files via upload

---
+++ CharacterPattern.java
@@ -0,0 +1,27 @@
+/*
+ Java code to print character pattern
+ A
+ B C 
+ D E F
+ G H I J
+ K L M N O
+ */
+
+import java.util.Scanner;
+
+public class CharacterPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows:");
+        int n = sc.nextInt();
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
