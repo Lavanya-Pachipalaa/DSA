7b068718970c58d399d1bcb858a3b9bacef36915
Lavanya-Pachipalaa <lavanyapachipala12@gmail.com>
2025-10-22 08:34:53 +0530

    Add files via upload

---
+++ HollowRectangle.java
@@ -0,0 +1,29 @@
+/*
+ Jav acode to print hollow rectangle pattern
+ * * * * *
+ *       *
+ *       *
+ * * * * *
+ */
+
+import java.util.Scanner;
+
+public class HollowRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows and number of columns:");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == m || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
